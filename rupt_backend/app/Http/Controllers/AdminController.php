ae40f7a9c6fe38c1c06613bd03865d1c5ff32a0a
Gilberto <gilberto1994@gmail.com>
2017-04-11 13:52:06 -0300

    Login Backend

---
+++ rupt_backend/app/Http/Controllers/AdminController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Admin;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use JWTAuth;
+
+class AdminController extends Controller
+{
+    public function signup(Request $request){
+        $this->validate($request,[
+            'name'  => 'required',
+            'email' => 'required|email|unique:admins',
+            'password' => 'required'
+        ]);
+
+        $admin = new Admin([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'))
+        ]);
+        $admin->save();
+
+        return response()->json([
+                'message'=>'Criado com sucesso!'
+            ],201);
+    }
+
+    public function signin(Request $request){
+        $this->validate($request,[
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        $credentials = $request->only('email', 'password');
+        try{
+            if(!$token = JWTAuth::attempt($credentials)){
+                return response()->json(['error' => 'Login ou senha incorretos'],401);
+            }
+        }catch(JWTException $e){
+            return response()->json(['error' => 'Login ou senha incorretos'],401);
+        }
+        return response()->json([
+            'token' => $token
+        ],200 );
+    }
+}
