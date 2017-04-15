41f1a74b11ecd3b29428604196dfd3b1ececfb1f
Murilo <murilo.ahb@gmail.com>
2017-04-15 18:25:12 -0300

    modal login

--- rupt-frontend/src/app/admin/login/login.component.ts
+++ rupt-frontend/src/app/admin/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+
+import {MaterializeAction} from 'angular2-materialize';
 
 @Component({
   selector: 'login',
@@ -10,6 +12,14 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  modalActions = new EventEmitter<string|MaterializeAction>();
+  openModal() {
+    this.modalActions.emit({action:"modal",params:['open']});
+  }
+  closeModal() {
+    this.modalActions.emit({action:"modal",params:['close']});
   }
 
 }
