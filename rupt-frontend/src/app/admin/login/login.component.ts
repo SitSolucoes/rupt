0069e53ee37cffbfb774438f56aa7a94fa142965
Murilo <murilo.ahb@gmail.com>
2017-04-16 23:35:02 -0300

    front login

--- rupt-frontend/src/app/admin/login/login.component.ts
+++ rupt-frontend/src/app/admin/login/login.component.ts
@@ -9,17 +9,41 @@ import {MaterializeAction} from 'angular2-materialize';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  modalActions = new EventEmitter<string|MaterializeAction>();
+
+  constructor() { 
+  }
 
   ngOnInit() {
   }
 
-  modalActions = new EventEmitter<string|MaterializeAction>();
-  openModal() {
+  ngAfterViewChecked() {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    this.openModal1();
+  }
+
+  ngAfterContentChecked() {
+    //Called after every check of the component's or directive's content.
+    //Add 'implements AfterContentChecked' to the class.
+    this.openModal1();
+  }
+
+  ngAfterContentInit() {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    this.openModal1();
+  }
+
+
+  
+  openModal1() {
     this.modalActions.emit({action:"modal",params:['open']});
+    console.log("open")
   }
   closeModal() {
     this.modalActions.emit({action:"modal",params:['close']});
+    console.log("close");
   }
 
 }
