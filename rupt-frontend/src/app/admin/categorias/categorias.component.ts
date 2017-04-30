e4d5398e852fc0dafb5f416c10300b9dd7634bd9
Murilo <murilo.ahb@gmail.com>
2017-04-30 12:40:33 -0300

    categorias

--- rupt-frontend/src/app/admin/categorias/categorias.component.ts
+++ rupt-frontend/src/app/admin/categorias/categorias.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NotificacoesService } from './../../services/notificacoes.service';
+
 @Component({
   selector: 'app-categorias',
   templateUrl: './categorias.component.html',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriasComponent implements OnInit {
 
-  constructor() { }
+  notificacoes;
+
+  constructor(private _notificacoesService: NotificacoesService) { }
 
   ngOnInit() {
+    this.notificacoes = this._notificacoesService.getNotificacoes();
   }
 
 }
