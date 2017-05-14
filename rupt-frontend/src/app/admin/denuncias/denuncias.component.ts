0ea6a25d1364db0526969fd09f4357c6fdbdb4b3
Murilo <murilo.ahb@gmail.com>
2017-05-14 15:07:16 -0300

    fix bug03

--- rupt-frontend/src/app/admin/denuncias/denuncias.component.ts
+++ rupt-frontend/src/app/admin/denuncias/denuncias.component.ts
@@ -31,7 +31,8 @@ export class DenunciasComponent implements OnInit {
 
   denuncias: Denuncia[] = [];
 
-  getList(){
+  getList(){return null};
+  /*getList(){
     this._denunciaService.getDenuncias()
       .subscribe(
         (denuncias: any) => {
@@ -46,9 +47,12 @@ export class DenunciasComponent implements OnInit {
                     this._postsService.getEscritor(p.autor_idLeitor).subscribe(
                         (data: any) => {
                         
-                          let leitor = new Leitor(data.leitor.id, data.leitor.nome, data.leitor.nick, data.leitor.sexo, 
+                          //NÃO MEXA NA PORRA DO MEU CONSTRUTOR CARALHO
+                          //let leitor = new Leitor(data.leitor.id, data.leitor.nome, data.leitor.nick, data.leitor.sexo, 
                                           data.leitor.nascimento, data.leitor.src_foto, data.leitor.email, data.leitor.password);
                           
+                          let leitor = new Leitor();
+
                           console.log(data.escritor.rg);
                           let escritor = new Escritor(leitor, data.escritor.rg, data.escritor.cpf, data.escritor.src_rg, data.escritor.src_cpf,
                                                       data.escritor.src_foto, data.escritor.biografia, data.escritor.banco, data.escritor.agecia, 
@@ -59,7 +63,9 @@ export class DenunciasComponent implements OnInit {
                           //getLeitor() // quem denunciou
                           this._denunciaService.getLeitor(denuncia.leitor_idLeitor).subscribe(
                             (ret: any) =>{
-                              let relator = new Leitor(ret.id, ret.nome, ret.nick, ret.sexo, ret.nascimento, ret.src_foto, ret.email, ret.password);
+                              
+                              //NÃO ALTERE A PORRA DO MEU CONSTRUTOR
+                              //let relator = new Leitor(ret.id, ret.nome, ret.nick, ret.sexo, ret.nascimento, ret.src_foto, ret.email, ret.password);
                               this._denunciaService.getMotivo(denuncia.motivo_idMotivo).subscribe(
                                 (mot: any) => {
                                   let motivo = new Motivo_Denuncia(mot.id, mot.motivo, mot.ativo, mot.created_at, mot.updated_at);
@@ -85,7 +91,7 @@ export class DenunciasComponent implements OnInit {
             }
           }
         );
-  }
+  }*/
 
   getPost(id): Post{
     console.log('antes');
