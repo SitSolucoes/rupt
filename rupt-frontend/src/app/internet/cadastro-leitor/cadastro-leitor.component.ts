16745d4ff79d7bba33b411eeb22e579d49bd23fc
Kaway Soares <kawaysoares@hotmail.com>
2017-10-07 22:53:03 -0300

    page cadastro

---
+++ rupt-frontend/src/app/internet/cadastro-leitor/cadastro-leitor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cadastro-leitor',
+  templateUrl: './cadastro-leitor.component.html',
+  styleUrls: ['./cadastro-leitor.component.css']
+})
+export class CadastroLeitorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
