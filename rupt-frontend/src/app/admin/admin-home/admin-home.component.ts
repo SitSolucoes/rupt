c116d527d6db9d7928058b344571381b1f280c10
Murilo <murilo.ahb@gmail.com>
2017-10-19 16:59:32 -0200

    categorias filtro 33%

--- rupt-frontend/src/app/admin/admin-home/admin-home.component.ts
+++ rupt-frontend/src/app/admin/admin-home/admin-home.component.ts
@@ -1,6 +1,8 @@
+import { AdministradoresService } from './../../services/administradores.service';
 import { Component, OnInit } from '@angular/core';
 
 import { NotificacoesService } from './../../services/notificacoes.service';
+import { Admin } from 'app/classes/admin';
 
 @Component({
   selector: 'app-admin-home',
@@ -8,14 +10,21 @@ import { NotificacoesService } from './../../services/notificacoes.service';
   styleUrls: ['./admin-home.component.css']
 })
 export class AdminHomeComponent implements OnInit {
-
+  admin: Admin = new Admin();
   notificacoes;
 
-  constructor(private _notificacoesService: NotificacoesService) { }
+  constructor(private _notificacoesService: NotificacoesService, 
+              private _adminService: AdministradoresService) { }
 
   admin_name: string = localStorage.getItem('adminLogado');
 
   ngOnInit() {
+    this._adminService.admin.subscribe(
+      (response) => { this.admin = response }
+    );
+
+    this._adminService.verificaLogin().subscribe();
+
     this.notificacoes = this._notificacoesService.getNotificacoes();
   }
 
