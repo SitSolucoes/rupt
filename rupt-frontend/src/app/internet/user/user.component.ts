98c7e5396caff2abf1d917f7eeaa31d6344978fb
Murilo <murilo.ahb@gmail.com>
2017-10-30 22:17:31 -0200

    perfil 70%

--- rupt-frontend/src/app/internet/user/user.component.ts
+++ rupt-frontend/src/app/internet/user/user.component.ts
@@ -15,7 +15,7 @@ import { Leitor } from 'app/classes/leitor';
 export class UserComponent implements OnInit {
   leitor: Leitor;
   leitorLogado: Leitor;
-  timeline: Timeline;
+  timeline: Timeline[] = new Array;
   url = ConnectionFactory.API_IMAGEM + 'profile/';
 
   constructor(private _activatedRoute: ActivatedRoute,
@@ -44,8 +44,63 @@ export class UserComponent implements OnInit {
 
   getTimeline(){
       this._timelineService.getTimeline(this.leitor.id).subscribe(
-        ( timeline: Timeline ) => { this.timeline = timeline }
+        ( timeline: Timeline[] ) => { this.timeline = timeline }
       )
+  }
+
+  getMonthString(month){
+    switch(month){
+      case 0:
+        return 'jan';
+      case 1:
+        return 'fev'  ;
+      case 2:
+        return 'mar';
+      case 3: 
+        return 'abr';
+      case 4: 
+        return 'mai';
+      case 5: 
+        return 'jun';
+      case 6:
+        return 'jul';
+      case 7:
+        return 'ago';
+      case 8: 
+        return 'set';
+      case 9:
+        return 'out';
+      case 10:
+        return 'nov';
+      case 11:
+        return 'dez';
+    }
+  }
+
+  calcHour(date){
+    let t = date.split(/[- :]/);
+    let result = new Date(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0);      
+
+    let today = new Date();
+
+    var timeDiff = Math.abs(today.getTime() - result.getTime());
+    var diffDays = (timeDiff / (1000 * 3600 * 24)); 
+
+    if (diffDays < 1){
+      let hour = Math.round(diffDays*24);
+      return hour.toString() + "h";
+    }
+    else if (diffDays <= 30){
+      let day = Math.round(diffDays);
+      return day.toString() + 'd';
+    }
+    else if (today.getFullYear == result.getFullYear){
+      return result.getDate().toString() + " " + this.getMonthString(result.getMonth());
+    }
+    else {
+      return result.getDate().toString() + " " + this.getMonthString(result.getMonth()) + ' de ' + result.getFullYear();
+    }
+
   }
 
 }
--- rupt_backend/app/Timeline.php
+++ rupt_backend/app/Timeline.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Timeline extends Model
 {
     public function post(){
-        return $this->hasOne('App\Post', 'id', 'post_idPost');
+        return $this->hasOne('App\Post', 'id', 'post_idPost')->with('autor');
     }
 }
