a6c62cba718602d868a0833ba929b74fb9ba5d66
Murilo <murilo.ahb@gmail.com>
2018-02-22 17:37:47 -0300

    denuncia adm + notificacao

--- rupt-frontend/src/app/admin/denuncias/denuncias.component.ts
+++ rupt-frontend/src/app/admin/denuncias/denuncias.component.ts
@@ -11,97 +11,97 @@ import { Option } from './../../shared/option';
 import { Post } from './../../classes/post';
 import { Leitor } from './../../classes/leitor';
 import { Motivo_Denuncia } from './../../classes/motivo_denuncia';
+import { ConnectionFactory } from '../../classes/connection-factory';
 
 @Component({
-  selector: 'app-denuncias',
-  templateUrl: './denuncias.component.html',
-  styleUrls: ['./denuncias.component.css']
+	selector: 'app-denuncias',
+	templateUrl: './denuncias.component.html',
+	styleUrls: ['./denuncias.component.css']
 })
 export class DenunciasComponent implements OnInit {
 
-  constructor(private _denunciaService: DenunciasService,
-              private _postsService: PostsService,
-              private _fb: FormBuilder) { }
+	url: string = ConnectionFactory.API_IMAGEM;
+	selectOptions: Option[] = [
+		{value: "-1", name: 'Sem Motivo'},
+	];
 
-  ngOnInit() {
-    this.getList();
-  }
-  selectOptions: Option[] = [
-    {value: "-1", name: 'Sem Motivo'},
-  ];
+	modalActions = new EventEmitter<string|MaterializeAction>();
 
-  modalActions = new EventEmitter<string|MaterializeAction>();
+	denuncia_selecionada = null;
+	post = null;
+	form: FormGroup;
+	
+	denuncias = [];
+	denunciasFiltro = [];
 
-  denuncia_selecionada = null;
-  post = null;
-  form: FormGroup;
-  
-  denuncias = [];
-  denunciasFiltro = [];
+	constructor(private _denunciaService: DenunciasService,
+				private _postsService: PostsService,
+				private _fb: FormBuilder) { }
 
-  //getList(){return null};
-  getList(){
-    this._denunciaService.getDenuncias()
-      .subscribe(
-        (denuncias: any) => {
-          this.denuncias = denuncias;
-          this.denunciasFiltro = denuncias;
-        }
-      );
-  }
+	ngOnInit() {
+		this.getList();
+	}
 
-  getPost(id): Post{
-    let post: Post;
-    this._denunciaService.getPost(id).subscribe(
-              (p: any) =>{
-                post = p[0];
-              }
-            );
-    return post;
-  }
+	//getList(){return null};
+	getList(){
+		this._denunciaService.getDenuncias()
+		.subscribe(
+			(denuncias: any) => {
+			this.denuncias = denuncias;
+			this.denunciasFiltro = denuncias;
+			}
+		);
+	}
 
-  closeModal(){
-      this.getList();
-      this.modalActions.emit({action:"modal",params:['close']});
-  }
+	getPost(id): Post{
+		let post: Post;
+		this._denunciaService.getPost(id).subscribe(
+				(p: any) =>{
+					post = p[0];
+				}
+				);
+		return post;
+	}
 
-  openModal(d){
-    this._denunciaService.getDetalhes(d).subscribe(
-      (ret: any) =>{
-        this.denuncia_selecionada = ret.denuncia;
-        this.montaForm();
-        this.modalActions.emit({action:"modal",params:['open']});
-      }
-    );
-  }
+	closeModal(){
+		this.getList();
+		this.modalActions.emit({action:"modal",params:['close']});
+	}
 
-  montaForm(){
-    this.form = this._fb.group({
-      idAdminDeleted: [localStorage.getItem('admin_id')],
-      deletedAt: [new Date()],
-      post_idPost: [this.denuncia_selecionada.post.id],
-      motivo_idMotivo: [this.denuncia_selecionada.detalhes.motivo_idMotivo]
-    });
-  }
-  
+	openModal(d){
+		this._denunciaService.getDetalhes(d).subscribe(
+		(ret: any) =>{
+			this.denuncia_selecionada = ret.denuncia;
+			this.montaForm();
+			this.modalActions.emit({action:"modal",params:['open']});
+		}
+		);
+	}
 
-  action(action){
-    if(action=="i"){
-      this._denunciaService.agir(this.form, 'i').subscribe(
-        (ret: any) => {
-          if(ret.status)
-            this.getList();
-        }
-      );
-    }else
-      this._denunciaService.agir(this.form, 'd').subscribe(
-        (ret: any) => {
-          if(ret.status)
-          this.getList();
-        }
-      );
-    
-  }
+	montaForm(){
+		this.form = this._fb.group({
+		idAdminDeleted: [localStorage.getItem('admin_id')],
+		deletedAt: [new Date()],
+		post_idPost: [this.denuncia_selecionada.post.id],
+		motivo_idMotivo: [this.denuncia_selecionada.detalhes.motivo_idMotivo]
+		});
+	}
+	
 
-
+	action(action){
+		if(action=="i"){
+			this._denunciaService.agir(this.form, 'i').subscribe(
+				(ret: any) => {
+				if(ret.status)
+					this.getList();
+				}
+			);
+		}else
+			this._denunciaService.agir(this.form, 'd').subscribe(
+				(ret: any) => {
+					if(ret.status)
+					this.getList();
+				}
+			);
+	}
 }
